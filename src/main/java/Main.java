a667c24cf9c1bb11a747af5eb745b72d0e59d0a6
costacamilly <169068284+costacamilly@users.noreply.github.com>
2024-05-13 14:37:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
+    System.out.println("☆Cantor Djonga ☆");
+    System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
+    System.out.println(" ");
+    System.out.println("*Álbum:Ladrão");
+  }
+}
