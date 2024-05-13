76f64b6d862eeac8f62f633fa2edcdecc1e468c1
costacamilly <169068284+costacamilly@users.noreply.github.com>
2024-05-13 15:17:01 +0000

    13/05/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
-    System.out.println("☆Cantor Djonga ☆");
+    System.out.println("☆Artista:Djonga☆");
     System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
     System.out.println(" ");
-    System.out.println("*Álbum:Ladrão");
+    System.out.println("-Álbum:Ladrão");
+    System.out.println(" ");
+    System.out.println("Ladrão é o terceiro álbum de estúdio do rapper brasileiro Djonga, lançado em 13 de março de 2019 através da gravadora Ceia");
+    System.out.println(" ");
+    System.out.println("-Faixas:");
+    System.out.println(" ");
+    System.out.println("1.	Hat-Trick                                       	4:19");
+    System.out.println("2.	Bené  	                                            3:49");
+    System.out.println("3.  Leal 	                                            3:33");
+    System.out.println("4.	Deus e o diabo na terra do sol -part. Filipe Ret    6:27");
+    System.out.println("5.	Tipo -part. MC Kaio                 	            3:49");
+    System.out.println("6.	Ladrão  	                                        4:22");
+    System.out.println("7.	Bença 	                                            3:44");
+    System.out.println("8.	Voz -part. Chris MC, Doug Now                    	5:12");
+    System.out.println ("9.	Mlk 4TR3V1D0                                     	1:16");
+    System.out.println("10.	Falcão 	                                            3:42");
+    System.out.println(" ");
+    System.out.println("-Duração Total: 40:13");
+ 
   }
 }
